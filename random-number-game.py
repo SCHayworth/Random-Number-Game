44bad441eb4e88fb1c886cba7753e9df63a909de
Shaun Hayworth <thismoderndeath@gmail.com>
2019-12-10 14:37:27 -0800

    Corrected syntax errors.

--- random-number-game.py
+++ random-number-game.py
@@ -18,24 +18,24 @@ def main():
     '''This is the mainline program logic.'''
     # Initialize accumulator for the total guesses, and a loop condition.
     guess_number = 0
-    correct_guess = false
+    correct_guess = False
 
     # Introduce the game and prompt the user for their first guess.
     print("I'm thinking of a number between 1 and 100.")
     guess = int(input('Can you guess what it is? Enter your guess: '))
 
     # Main gameplay loop.
-    while correct_guess == false:
+    while correct_guess == False:
         # Increment the guess_number accumulator and check for a correct guess.
         # If the guess is wrong, prompt user for a new number.
         guess_number +=1
         if guess == target_number:
             if guess_number == 1:
                 print(f"Yes, my number is {target_number}! You got it on the first try!")
-                correct_guess = true
+                correct_guess = True
             else:
                 print(f"Boom!! You got it in {guess_number} guesses! My number was {target_number}.")
-                correct_guess = true
+                correct_guess = True
         elif guess < target_number:
             guess = int(input(f"{guess} is too low. Try a larger number: "))
         else:
