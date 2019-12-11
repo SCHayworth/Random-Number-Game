3a3d427851e023359d750411475a1ebb7f16f4e0
Shaun Hayworth <thismoderndeath@gmail.com>
2019-12-10 18:58:45 -0800

    Adjusted exception catching to make the game continue instead of aborting the program.

--- random-number-game.py
+++ random-number-game.py
@@ -20,36 +20,43 @@ def main():
     guess_number = 0
     correct_guess = False
 
-    try:
-        # Introduce the game and prompt the user for their first guess.
-        print("I'm thinking of a number between 1 and 100.")
-        guess = int(input('Can you guess what it is? Enter your guess: '))
+    # Introduce the game and prompt the user for their first guess as long as
+    # the guess_number accumulator is 0.
+    while guess_number < 1:
+        try:
+            print("I'm thinking of a number between 1 and 100.")
+            guess = int(input('Can you guess what it is? Enter your guess: '))
 
-        # Main gameplay loop.
-        while correct_guess == False:
-            try:
-                # Increment the guess_number accumulator and check for a correct guess.
-                # If the guess is wrong, prompt user for a new number.
-                guess_number +=1
-                if guess == target_number:
-                    if guess_number == 1:
-                        print(f"\nYes, my number is {target_number}! You got it on the first try!")
-                        correct_guess = True
+            # Main gameplay loop.
+            while correct_guess == False:
+                try:
+                    # Increment the guess_number accumulator and check for a correct guess.
+                    # If the guess is wrong, prompt user for a new number.
+                    guess_number +=1
+                    if guess == target_number:
+                        if guess_number == 1:
+                            print(f"\nYes, my number is {target_number}! You got it on the first try!")
+                            correct_guess = True
+                        else:
+                            print(f"\nBoom!! You got it in {guess_number} guesses! My number was {target_number}.")
+                            correct_guess = True
+                    elif guess < target_number and guess >= 1:
+                        guess = int(input(f"{guess} is too low. Try a larger number: "))
+                    elif guess > target_number and guess <= 100:
+                        guess = int(input(f"{guess} is too high. Try a smaller number: "))
                     else:
-                        print(f"\nBoom!! You got it in {guess_number} guesses! My number was {target_number}.")
-                        correct_guess = True
-                elif guess < target_number and guess >= 1:
-                    guess = int(input(f"{guess} is too low. Try a larger number: "))
-                elif guess > target_number and guess <= 100:
-                    guess = int(input(f"{guess} is too high. Try a smaller number: "))
-                else:
-                    guess = int(input('Please choose a number between 1 and 100.'))
-            except ValueError:
-                print('You must enter an integer between 1 and 100. Try again.\n')
-                continue
-    except ValueError:
-        print('You must enter an integer between 1 and 100.\n')
-        break
+                        # If the selected number is out of range, ask for a valid
+                        # guess and decrement the guess_number in order to not count it.
+                        guess = int(input('Please choose a number between 1 and 100: '))
+                        guess_number -= 1
+                except ValueError:
+                    # If a the input is not an integer, prompt the user to try again and continue the game.
+                    print('You must enter an integer between 1 and 100. Try again.\n')
+                    continue
+        except ValueError:
+            # If the first guess is not an integer, prompt the user for an appropriate value.
+            print('You must enter an integer between 1 and 100.\n')
+            continue
 
 
 # Call the main() function to execute program.
